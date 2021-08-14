49cbcd512c3e00f1783bceb6a4353d79a35ae343
vanTamBachKhoa <devtam@users.noreply.github.com>
2021-08-14 17:35:52 +0700

    fix deplor

--- vnpay_php/config.php
+++ vnpay_php/config.php
@@ -5,7 +5,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-$vnp_TmnCode = "0NMB5MRO"; //Mã website tại VNPAY 
-$vnp_HashSecret = "YFTZHKRQNNVFYLYMWFJDQBVZDMOTCQMV"; //Chuỗi bí mật
+// $vnp_TmnCode = "0NMB5MRO"; //Mã website tại VNPAY 
+// $vnp_HashSecret = "YFTZHKRQNNVFYLYMWFJDQBVZDMOTCQMV"; //Chuỗi bí mật
+$vnp_TmnCode = "Y4U88XFK"; //Mã website tại VNPAY 
+$vnp_HashSecret = "DTHXNFNBUMNKFKQOZVHTXUXNUQUUXMTV"; //Chuỗi bí mật
 $vnp_Url = "http://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
 $vnp_Returnurl = "https://the-closer.herokuapp.com/vnpay_php/vnpay_return.php";
